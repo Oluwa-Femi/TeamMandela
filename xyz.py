5b620c62fe9b4fd3ca6e0c15ac1080272c18ff25
oluwafemi oluwatola <oluwafemi.oluwatola@sterling.ng>
2021-09-09 18:10:19 +0100

    Added age as a parameter

--- xyz.py
+++ xyz.py
@@ -1,7 +1,7 @@
-def team_member (name):
+def team_members (name,age):
     if name == "Femi":
         print("Hello")
     else:
         print("You are not authorized!!")
 
-team_member("Femi")
+team_member("Femi", 12)
